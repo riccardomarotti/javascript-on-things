7ed2d7143be380d1bc68aea778ff4919010093d3
Riccardo <riccardo.marotti@gmail.com>
2017-03-26 15:24:31 +0200

    Use RxJS to manage events in timer: no more globals

--- timer-advanced.js
+++ timer-advanced.js
@@ -1,129 +1,86 @@
+Rx = require("rx");
+
 var five = require('johnny-five');
 var board = new five.Board();
 
+const DEFAULT_TIMER = 10;
 
-const DEFAULT_TIMER = 10000;
-const UPPER_LIMIT = 99 * 60000;
-const LOWER_LIMIT = 1000;
-var timeRemaining;
-var timer;
-var timeString, lastTimeString;
-var timestamp, lastTimestamp;
-
-
-board.on('ready', () => {
-  var downButton = new five.Button(2);
-  var upButton = new five.Button(3);
-  var goButton = new five.Button(4);
-  var lcd = new five.LCD([7, 8, 9, 10, 11, 12]);
-  var alertChime = new five.Piezo(6);
-
-
-
-  function init () {
-    timeRemaining = DEFAULT_TIMER;
-    lastTimeString = '00:00';
-    timeString = '';
-    showRemaining();
-  }
-
-
-  function showRemaining () {
-    var minutes, seconds, minPad, secPad;
-    minutes = Math.floor(timeRemaining / 60000);
-    seconds = Math.floor((timeRemaining % 60000) / 1000);
-    minPad = (minutes < 10) ? '0' : '';
-    secPad = (seconds < 10) ? '0' : '';
-    timeString = `${minPad}${minutes}:${secPad}${seconds}`;
-    if (timeString != lastTimeString) {
-      lcd.cursor(0, 0).print(timeString);
-    }
-  }
-
-
-  function adjustTime (delta) {
-    timeRemaining += delta;
-    if (timeRemaining < LOWER_LIMIT) {
-      timeRemaining = LOWER_LIMIT;
-    } else if (timeRemaining > UPPER_LIMIT) {
-      timeRemaining = UPPER_LIMIT;
-    }
-    showRemaining();
-  }
-
-  function start () {
-    lcd.clear();
-    timestamp = Date.now();
-    timer = setInterval(tick, 250);
-    tick();
-  }
-
-  function tick () {
-    lastTimestamp = timestamp;
-    timestamp = Date.now();
-    timeRemaining -= (timestamp - lastTimestamp);
-    if (timeRemaining <= 0) {
-      timer = clearInterval(timer);
-      chime();
-      init();
-    }
-
-    showRemaining();
-  }
-
-  function pause () {
-    timer = clearInterval(timer);
-    lcd.cursor(0, 9).print('PAUSED');
-  }
-
-  function chime () {
+function playChime (alertChime, lcd) {
     alertChime.play({
-      tempo: 120,
-      song: [
-        ['e5', 1],
-        ['g#5', 1],
-        ['f#5', 1],
-        ['b4', 2],
-        ['e5', 1],
-        ['f#5', 1],
-        ['g#5', 1],
-        ['e5', 2],
-        ['g#5', 1],
-        ['e5', 1],
-        ['f#5', 1],
-        ['b4', 2],
-        ['b4', 1],
-        ['f#5', 1],
-        ['g#5', 1],
-        ['e5', 2]
-      ]
+        tempo: 120,
+        song: [
+            ['e5', 1],
+            ['g#5', 1],
+            ['f#5', 1],
+            ['b4', 2],
+            ['e5', 1],
+            ['f#5', 1],
+            ['g#5', 1],
+            ['e5', 2],
+            ['g#5', 1],
+            ['e5', 1],
+            ['f#5', 1],
+            ['b4', 2],
+            ['b4', 1],
+            ['f#5', 1],
+            ['g#5', 1],
+            ['e5', 2]
+        ]
     });
     lcd.cursor(0, 9).print('DONE!');
-  }
+}
+
+function getStringTime(x) {
+    var minutes, seconds, minPad, secPad;
+    minutes = Math.floor(x / 60);
+    seconds = Math.floor(x % 60);
+    minPad = (minutes < 10) ? '0' : '';
+    secPad = (seconds < 10) ? '0' : '';
+    return `${minPad}${minutes}:${secPad}${seconds}`;
+}
 
 
 
 
-  downButton.on('press', () => { // remove a second
-    adjustTime(-1000);
-  });
+const ready$ = Rx.Observable.fromEvent(board, 'ready');
+ready$.subscribe(() => {
+    var downButton = new five.Button(3);
+    var upButton = new five.Button(4);
+    var goButton = new five.Button(2);
+    var lcd = new five.LCD([7, 8, 9, 10, 11, 12]);
+    var alertChime = new five.Piezo(6);
 
-  upButton.on('press', () => { // add a second
-    adjustTime(1000);
-  });
+    const tick$ = Rx.Observable.interval(1000);
+    const startPause$ = Rx.Observable.fromEvent(goButton, 'press');
+    const up$ = Rx.Observable.fromEvent(upButton, 'press');
+    const down$ = Rx.Observable.fromEvent(downButton, 'press');
 
-  goButton.on('press', () => {
-    if (!timer) {
-      start();
-    } else {
-      pause();
-    }
-  });
+    const decrement = (acc) => ({count: acc.count - 1, counting: acc.counting});
+    const increment = (acc) => ({count: acc.count + 1, counting: acc.counting});
+    const doNothing = (acc) => ({count: acc.count, counting: acc.counting})
+    const switchCounting = (acc) => ({count: acc.count, counting: !acc.counting})
+    const updateCounterIfCounting = (acc) => {
+        if(acc.counting) {
+            return decrement(acc);
+        } else {
+            return doNothing(acc);
+        }
+    };
 
+    timer$ = Rx.Observable.merge(
+        tick$.map(() => updateCounterIfCounting),
+        startPause$.map(() => switchCounting),
+        up$.map(() => increment),
+        down$.map(() => decrement)
+    )
+    .startWith({count: DEFAULT_TIMER, counting: false})
+    .scan((acc, curr) => curr(acc));
 
-  init();
-
-
-
-
+    timer_end$ = timer$.filter((x) => x.count <= 0);
+    timer$.takeUntil(timer_end$).subscribe(
+        (x) => lcd.cursor(0, 0).print(getStringTime(x.count)),
+        (err) => lcd.cursor(0, 0).print(err),
+        () => playChime(alertChime, lcd)
+    );
 });
+
